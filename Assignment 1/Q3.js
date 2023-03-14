a346d4bf2d2afacf8802d88b5964df2bcd3bfff7
Akshadachobhee <akshadachobhe7@gmail.com>
2023-03-14 23:01:37 +0530

    Factors of perticular number : Still Want to  know about how to print data vertically

---
+++ Assignment 1/Q3.js
@@ -0,0 +1,14 @@
+/*Write a typescript program which contains one function named as DisplayFators.
+This function should accept one number and display factors of that number.
+Input : 20
+Output : 1 2 4 5 10 */
+function DisplayFators(no) {
+    var i;
+    for (i = 1; i < no; i++)
+        if (no % i == 0) {
+            console.log(i);
+        }
+    return 0;
+    // if return i = it will print 20 alo
+}
+var ret = DisplayFators(20);
