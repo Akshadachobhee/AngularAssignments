30ad4783acdde126d10348717f7718c400f9fbe5
Akshadachobhee <akshadachobhe7@gmail.com>
2023-03-14 22:04:22 +0530

    Area Of circle / Still need Input from user

--- Assignment 1/Q2.js
+++ Assignment 1/Q2.js
@@ -1,13 +1,10 @@
 /* Write a typescript program which contains one function named as Area.
- That function should calculate area of circle. 
+ That function should calculate area of circle.
  Accept value of radius from user and return its area.
  Default value of PI should be 3.14 if it is not provided by the caller */
- function Area(radius) : number
- { 
-    
- var AOC = 3.14 * radius *radius
-  return AOC 
- }
- var ret = Area(5)
-
- console.log("Area of circle is "+ret)
+function Area(radius) {
+    var AOC = 3.14 * radius * radius;
+    return AOC;
+}
+var ret = Area(5);
+console.log("Area of circle is " + ret);
