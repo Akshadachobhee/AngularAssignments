b818b0c63f7901968cad78c0a726370af0222ec4
Akshadachobhee <akshadachobhe7@gmail.com>
2023-03-15 22:43:36 +0530

    will need to know the logic

---
+++ Assignment 2/Q3.ts
@@ -0,0 +1,12 @@
+/*Write a typescript program which contains one function named Maximum. 
+That function accepts array of number and returns the second largest number from array.
+Input : 23 89 6 29 56 45 77 32
+Output : Second Maximum number is 77
+ */
+function SecondMaximum(Array : number[]) : number
+{
+    return 0
+}
+var arr : number[] = [23 ,89, 6 ,29 ,56, 45 ,77 ,32]
+var ret = 0,
+ret = SecondMaximum(arr)
