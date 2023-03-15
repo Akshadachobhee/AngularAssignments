f3793c90349c5b284b77834acb6a9cd32e2ae1fd
Akshadachobhee <akshadachobhe7@gmail.com>
2023-03-15 21:09:51 +0530

    Prime number or not // In

---
+++ Assignment 1/Q4.ts
@@ -0,0 +1,21 @@
+/* Write a typescript program which contains one function named as ChkPrime.
+That function should accept one number and it should return true if the given number is prime and otherwise return false 
+Input : 11
+Output : It is a prime number */
+function Chkprime(no) : number
+{  
+   var i,flag=true;
+  for(i=2;i<=no-1;i++)
+  if(no%i==0)
+{
+  flag = false
+  break
+}
+if(flag == true)
+console.log("It is a Prime number")
+else
+console.log("It is not a Prime number")
+return 0
+}
+
+var ret = Chkprime(11)
