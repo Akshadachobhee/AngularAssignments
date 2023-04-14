99849647f975b48f20fe3f7be490a90abbf5929c
Akshadachobhee <akshadachobhe7@gmail.com>
2023-04-14 21:35:37 +0530

    Assignment 6

---
+++ Assignment 6/QueSrc/src/app/new-comp/new-comp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-comp',
+  templateUrl: './new-comp.component.html',
+  styleUrls: ['./new-comp.component.css']
+})
+export class NewCompComponent 
+{
+name = "Marvellous"
+concat = "Infosystems"
+}
