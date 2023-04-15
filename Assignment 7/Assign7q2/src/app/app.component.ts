f2cfa4928238d76f24567476362aa9e4e4b8affd
Akshadachobhee <akshadachobhe7@gmail.com>
2023-04-15 16:01:03 +0530

    Assignment 7 ... Need to see q3 &4

---
+++ Assignment 7/Assign7q2/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent
+{
+  data : String ='';
+  fun()
+  {
+   return "Marvellous infosystem"
+  }
+
+}
